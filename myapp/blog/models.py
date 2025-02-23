df3d01eac4353402bb5084ee17c1de0c002b09e7
mukkss <muksss1102@gmail.com>
2025-02-23 19:57:33 +0530

    CreatingMigrations_ReferModel.py_Comments

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
     def __init__(self):
         pass
     title = models.CharField(max_length=100)
     content = models.TextField()
-    img_url = models.URLField(blank=True)
+    img_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
