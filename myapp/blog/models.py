185682dc3f075420c7dcc2acda868bb7caacc122
mukkss <muksss1102@gmail.com>
2025-02-23 19:39:13 +0530

    CreatingModels

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    def __init__(self):
+        pass
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    img_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
