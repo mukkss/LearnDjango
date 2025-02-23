f7b9d39e3a8154a97c045b186088fd6eaab8d14c
mukkss <muksss1102@gmail.com>
2025-02-23 08:04:38 +0530

    DynamicDataUpdateUsingForSyntax

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,7 +5,25 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     blog_title = 'Latest Posts'
-    return render(request, 'blog/index.html', {'blog_title': blog_title})
+    posts = [
+        {
+            'title': 'Post 1',
+            'content': 'This is the first post'
+        },
+        {
+            'title': 'Post 2',
+            'content': 'This is the second post'
+        },
+        {
+            'title': 'Post 3',
+            'content': 'This is the third post'
+        },
+        {
+            'title': 'Post 4',
+            'content': 'This is the fourth post'
+        },
+    ]
+    return render(request, 'blog/index.html', {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
     return render(request, 'blog/details.html')
