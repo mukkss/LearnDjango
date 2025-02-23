dbb70add90b6b66636c14848166a9b4ce6ead083
mukkss <muksss1102@gmail.com>
2025-02-23 08:35:58 +0530

    UrlTag_And_DunamicUrl

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -7,18 +7,22 @@ def index(request):
     blog_title = 'Latest Posts'
     posts = [
         {
+            'id': 1,
             'title': 'Post 1',
             'content': 'This is the first post'
         },
         {
+            'id': 2,
             'title': 'Post 2',
             'content': 'This is the second post'
         },
         {
+            'id': 3,
             'title': 'Post 3',
             'content': 'This is the third post'
         },
-        {
+        {  
+            'id': 4,
             'title': 'Post 4',
             'content': 'This is the fourth post'
         },
