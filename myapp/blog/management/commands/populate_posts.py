4a81a4d50c42381a01037be8e490dc52585657a4
mukkss <muksss1102@gmail.com>
2025-05-02 19:43:54 +0530

    feat: add category field to Post model and update data population command

--- myapp/blog/management/commands/populate_posts.py
+++ myapp/blog/management/commands/populate_posts.py
@@ -1,8 +1,7 @@
-
 from typing import Any
-from blog.models import Post
+from blog.models import Post, Category
 from django.core.management.base import BaseCommand
-
+import random
 
 
 class Command(BaseCommand):
@@ -11,6 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
         #Deleteing Exsisting Data
         Post.objects.all().delete()
+
         titles = [
             "The Future of AI",
             "Climate Change Solutions",
@@ -80,7 +80,10 @@ class Command(BaseCommand):
             "https://picsum.photos/id/20/800/400",
         ]
 
+        
+        categories = Category.objects.all()
         for title, content, img_url in zip(titles, contents, img_urls):
-            Post.objects.create(title=title, content=content,img_url=img_url)
+            category = random.choice(categories)
+            Post.objects.create(title=title, content=content,img_url=img_url, category=category)
 
         self.stdout.write(self.style.SUCCESS("Completed Inserting Data!"))
